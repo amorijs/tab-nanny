c2c6c7b2130b89b0628eca127563e86ee5b9c7c6
Chris Amori <chris.amori93@gmail.com>
2017-06-21 23:57:42 -0700

    test

--- background.js
+++ background.js
@@ -1,11 +1,25 @@
 const virtualTabs = [];
 let previousTabIndex;
 
-const sortByLastUsed = (array) => array.sort((a, b) => a.lastUsed - b.lastUsed);
+const sortByLastUsed = (array) => array.slice(0).sort((a, b) => a.lastUsed - b.lastUsed);
 
 const moveTabs = (virtTabs) => {
-  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
-    setTimeout(() => chrome.tabs.move(tabs[0].id, { index: 0 }), 125);
+  // chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+  //   setTimeout(() => chrome.tabs.move(tabs[0].id, { index: 0 }), 125);
+  // });
+  const sortedVirtualTabs = sortByLastUsed(virtualTabs);
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    sortedVirtualTabs.forEach((virtTab, index) => {
+      let i = 0;
+      let activeTabId;
+      tabs.forEach((tab) => {
+        i += 500;
+        if (virtTab.id === tab.id) setTimeout(() => chrome.tabs.move(tab.id, { index }), i);
+        if (tab.active) activeTabId = tab.id;
+      });
+      // i += 500;
+      // setTimeout(() => chrome.tabs.move(activeTabId, { index: -1 }), i);
+    });
   });
 };
 
@@ -24,6 +38,8 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
     }
 
     previousTabIndex = tabs[0].index;
-    moveTabs();
+    // moveTabs();
   });
 });
+
+setTimeout(moveTabs, 10000);
