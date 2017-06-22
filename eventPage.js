e90e24ef3d7b21451598ae69ac7a315ed635f051
Chris Amori <chris.amori93@gmail.com>
2017-06-22 15:51:21 -0700

    MVP

--- eventPage.js
+++ eventPage.js
@@ -1,13 +1,67 @@
-console.log('test');
+let virtualTabs = [];
+let previousTabIndex;
+const leftmp3 = new Audio();
+leftmp3.src = 'totheleft.mp3';
 
-// chrome.tabs.onCreated.addListener(() => {
-//   chrome.tabs.query({}, (tabs) => {
-//     chrome.tabs.move(tabs[1].id, { index: -1 });
-//   });
-// });
+// create virtual tabs on start
+const initializeVirtualTabs = () => {
+  virtualTabs = [];
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    tabs.forEach(tab => {
+      tab.lastUsed = Date.now();
+      virtualTabs.push(tab);
+    });
+  });
+};
 
-chrome.tabs.onActivated.addListener((activeInfo) => {
-  chrome.tabs.query({}, (tabs) => {
-    chrome.tabs.move(tabs[1].id, { index: -1 });
+// query current active window, set equal to prevTabIndex
+const initializePreviousTabIndex = () => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    previousTabIndex = tabs[0].index;
+  });
+};
+
+initializePreviousTabIndex();
+initializeVirtualTabs();
+
+// LISTENERS
+chrome.browserAction.onClicked.addListener(() => {
+  // get current active tab and make sure its last used is set before sorting
+  // query is async, this call will not finish before any calls outside of query
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    virtualTabs[tabs[0].index].lastUsed = Date.now();
+    console.log('before sort:', virtualTabs);
+    sortByLastUsed(virtualTabs);
+    console.log('after sort:', virtualTabs);
+    moveTabs();
   });
 });
+
+chrome.tabs.onActivated.addListener((activeInfo) => {
+  chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
+    virtualTabs[previousTabIndex].lastUsed = Date.now();
+    previousTabIndex = tabs[0].index;
+  });
+});
+
+// chrome.tabs.onMoved.addListener(() => {
+//   initializePreviousTabIndex();
+//   initializeVirtualTabs();
+// });
+
+const sortByLastUsed = (array) => array.sort((a, b) => b.lastUsed - a.lastUsed);
+
+const moveTabs = (virtTabs) => {
+  leftmp3.play();
+  chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    virtualTabs.forEach((virtTab, index) => {
+      tabs.forEach((tab) => {
+        if (virtTab.id === tab.id) {
+          chrome.tabs.move(tab.id, { index });
+        }
+      });
+    });
+
+    initializePreviousTabIndex();
+  });
+};
