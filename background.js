edfeb644d963451fb84c9d67eb6a788ac8978f15
Chris Amori <chris.amori93@gmail.com>
2017-06-22 01:59:52 -0700

    Beyonce

--- background.js
+++ background.js
@@ -1,9 +1,12 @@
 const virtualTabs = [];
 let previousTabIndex;
+const leftmp3 = new Audio();
+leftmp3.src = 'totheleft.mp3';
 
 const sortByLastUsed = (array) => array.sort((a, b) => a.lastUsed - b.lastUsed);
 
 const moveTabs = (virtTabs) => {
+  leftmp3.play();
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     setTimeout(() => chrome.tabs.move(tabs[0].id, { index: 0 }), 125);
   });
