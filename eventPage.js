3f49be3fa05c29daedf7da544eb96be6f5bcf7b9
Chris Amori <chris.amori93@gmail.com>
2017-06-22 18:14:20 -0700

    Add listeners for create remove attach and detach

--- eventPage.js
+++ eventPage.js
@@ -1,21 +1,29 @@
 let virtualTabs = [];
 let previousTabIndex;
+let currentlyMoving = false;
 const leftmp3 = new Audio();
 leftmp3.src = 'totheleft.mp3';
 
 // create virtual tabs on start
 const initializeVirtualTabs = () => {
+  if (currentlyMoving) return;
+
   virtualTabs = [];
   chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    console.log('BEFORE:', virtualTabs);
     tabs.forEach(tab => {
       tab.lastUsed = Date.now();
       virtualTabs.push(tab);
     });
+    console.log('AFTER:', virtualTabs);
   });
 };
 
 // query current active window, set equal to prevTabIndex
 const initializePreviousTabIndex = () => {
+  console.log('in initiprevtabind, currentlyMoving: ', currentlyMoving);
+  if (currentlyMoving) return;
+
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     previousTabIndex = tabs[0].index;
   });
@@ -26,13 +34,9 @@ initializeVirtualTabs();
 
 // LISTENERS
 chrome.browserAction.onClicked.addListener(() => {
-  // get current active tab and make sure its last used is set before sorting
-  // query is async, this call will not finish before any calls outside of query
   chrome.tabs.query({ active: true, currentWindow: true }, (tabs) => {
     virtualTabs[tabs[0].index].lastUsed = Date.now();
-    console.log('before sort:', virtualTabs);
     sortByLastUsed(virtualTabs);
-    console.log('after sort:', virtualTabs);
     moveTabs();
   });
 });
@@ -44,24 +48,52 @@ chrome.tabs.onActivated.addListener((activeInfo) => {
   });
 });
 
-// chrome.tabs.onMoved.addListener(() => {
-//   initializePreviousTabIndex();
-//   initializeVirtualTabs();
-// });
+chrome.tabs.onMoved.addListener(() => {
+  initializePreviousTabIndex();
+  initializeVirtualTabs();
+});
+
+chrome.tabs.onCreated.addListener(() => {
+  initializePreviousTabIndex();
+  initializeVirtualTabs()
+});
+
+chrome.tabs.onRemoved.addListener(() => {
+  initializePreviousTabIndex();
+  initializeVirtualTabs()
+});
+
+chrome.tabs.onAttached.addListener(() => {
+  initializePreviousTabIndex();
+  initializeVirtualTabs()
+});
+
+chrome.tabs.onDetached.addListener(() => {
+  initializePreviousTabIndex();
+  initializeVirtualTabs()
+});
 
 const sortByLastUsed = (array) => array.sort((a, b) => b.lastUsed - a.lastUsed);
 
 const moveTabs = (virtTabs) => {
   leftmp3.play();
   chrome.tabs.query({ currentWindow: true }, (tabs) => {
+    currentlyMoving = true;
+    const promises = [];
     virtualTabs.forEach((virtTab, index) => {
       tabs.forEach((tab) => {
         if (virtTab.id === tab.id) {
-          chrome.tabs.move(tab.id, { index });
+          let currentIndex = promises.length;
+          promises.push(new Promise((resolve, reject) => {
+            chrome.tabs.move(tab.id, { index }, () => resolve());
+          }));
         }
       });
     });
-
-    initializePreviousTabIndex();
+    Promise.all(promises).then(() => {
+      currentlyMoving = false;
+      console.log(virtualTabs);
+      initializePreviousTabIndex();
+    });
   });
 };
